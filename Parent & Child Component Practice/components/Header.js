37aeed4134670d90beb5b9c9dc05cd6a552054a1
Matthew Tseng <tsengmatthew@hotmail.com>
2019-04-18 19:12:50 -0700

    finished Parent & Child Components Practice

---
+++ Parent & Child Component Practice/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+	return (
+		<header>
+			"This contains the header information."
+		</header>
+	)
+}
+
+export default Header
--- Parent & Child Component Practice/index.js
+++ Parent & Child Component Practice/index.js
@@ -5,3 +5,19 @@
   // 2. A MainContent component
   // 3. A Footer component
 
+// Will create a file hierarchy of this
+//				index.js
+//					|
+//				  App.js
+//			/		|		\
+//	 Header.js MainContent.js Footer.js
+// 
+// All files that are below a file will need to be exported
+// and will be imported by the one above.
+
+import React from "react"
+import ReactDOM from "react-dom"
+// Do not necessarily have to put App.js 
+import App from "./App"
+
+ReactDOM.render(<App />, document.GetElementById("root"))
